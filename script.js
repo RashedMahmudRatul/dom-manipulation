9ccc535e1014acfa16a799f9aa289e49e4236a3c
Rashed Mahmud Ratul <rashedmahmud689@gmail.com>
2018-10-15 17:43:44 +0000

    Task4

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
 /* Task 1 */
+
 var heading=document.getElementById('heading');
 console.log(heading.innerText);
-heading.innerText="Changed it";
-/*heading.innerHTML=<h3>"Changed it"</h3>;
+/*heading.innerText="Changed it";*/
+/*heading.innerHTML=<h3>"Changed it"</h3>;*/
 /* Task 2 using plain JavaScript */
 var textBtn=document.getElementById('addText2');
 textBtn.addEventListener('click',function(event){
@@ -23,5 +24,26 @@ function changeBGcolor(event){
     }
 }
 
+function redirectToGoogle(event){
+    window,location.assign("https://www.google.com");
+}
 
-/* Task 4 using jQuery */
+/* Task 4a using jQuery */
+$('#addText4').click(function(event){
+    var paragraph = $('<p>').text('Hello world');
+    $('#task4a').append(paragraph);
+    
+});
+
+/* Task 4b using jQuery */
+function changeBG(event){
+    var color=event.target.innerText.toLocaleLowerCase();
+    $('body').css('background-color',color);
+}
+
+/* Task 4c using jQuery */
+
+$("#sum").click(function(event){
+    var sum=Number($("#num1").val())+Number($("#num2").val());
+    $('#result').text(sum);
+});
